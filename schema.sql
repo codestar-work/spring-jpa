c4493db1709c075699a0f93b308e1a22fa18da5c
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-06-11 18:37:24 +0700

    Add more info to schema

--- schema.sql
+++ schema.sql
@@ -5,8 +5,17 @@ grant all on airport.* to 'airport'@'localhost';
 
 use airport;
 create table airport (
-  id   serial,
-  code nvarchar(64)
+  id      serial,
+  code    nvarchar(64),
+  name    nvarchar(200),
+  city    nvarchar(200),
+  country nvarchar(200)
 );
 
-insert into airport(code) values('BKK');
+insert into airport(code, name, city, country)
+values('BKK', 'Suvarnabhumi', 'Bangkok', 'Thailand');
+insert into airport(code, name, city, country)
+values('DMK', 'Don Muang', 'Bangkok', 'Thailand');
+insert into airport(code, name, city, country)
+values('CNX', 'Chiang Mai', 'Chiang Mai', 'Thailand');
+
