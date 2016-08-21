3c393f4044c315739b9245eb52b905abd72a765d
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-08-21 10:36:41 +0700

    Refactor JDBC

--- src/main/java/web/Sample.java
+++ src/main/java/web/Sample.java
@@ -1,35 +1,36 @@
 package web;
 import java.sql.*;
 import java.util.*;
-import org.springframework.ui.*;
-import org.springframework.jdbc.core.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.*;
 
 @Controller
 public class Sample {
 
-	@RequestMapping("/data") @ResponseBody
-	List data() {
-		List list = new LinkedList<Coffee>();
-		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			Connection cn = DriverManager.getConnection(
-				"jdbc:mysql://localhost/coffee_shop?characterEncoding=UTF-8" +
-				"&user=coffee&password=password");
-			PreparedStatement ps = cn.prepareStatement("select * from coffee");
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				Coffee c = new Coffee();
-				c.name   = rs.getString("name");
-				c.price  = rs.getDouble("price");
-				list.add(c);
-			}
-			rs.close();
-			ps.close();
-			cn.close();
-		} catch (Exception e) { }
-		return list;
-	}
+@RequestMapping("/data") @ResponseBody
+List data() {
+	List list = new LinkedList<Coffee>();
+	String sql = "select * from coffee";
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection cn = DriverManager.getConnection(url);
+		PreparedStatement ps = cn.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			Coffee c = new Coffee();
+			c.name   = rs.getString("name");
+			c.price  = rs.getDouble("price");
+			list.add(c);
+		}
+		rs.close();
+		ps.close();
+		cn.close();
+	} catch (Exception e) { }
+	return list;
+}
+
+String url = "jdbc:mysql://localhost/coffee_shop" +
+	"?characterEncoding=UTF-8" +
+	"&user=coffee&password=password";
+
 }
